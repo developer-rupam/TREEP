b581b00fbfa5248667e8c956c9aa838b0454df89
developer-rupam <developer.rupam@gmail.com>
2021-07-23 02:42:13 +0530

    node & moongoose setup

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+//const adminUserController = require('../api/admin/users/users.controller')
+//const adminServiceController = require('../api/admin/services/services.controller')
+
+//router.use('/admin/users/',adminUserController);
+//router.use('/admin/services/',adminServiceController);
+
+
+
+module.exports = router
